c7f60756717eec45a267db33226b049279f01a33
Edouard Poitras <eddie@quantumcyberdefence.com>
2021-05-09 22:40:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# T2DTheme
+
+A dark Aurora theme.
+
+## Details
+
+Changes include:
+
+1) Black background
+2) White text
+3) Bumped font up to 11
+
+## Dependencies
+
+Requires the ThemeCreator patch.
+
+## TODO
+
+Need to polish it up and handle various corner cases that don't quite look good yet.
---
+++ T2DTheme.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+
+using HarmonyLib;
+
+namespace T2DTheme
+{
+    public class T2DTheme : AuroraPatch.Patch
+    {
+        public override string Description => "twice2double's Dark Aurora Theme";
+        public override IEnumerable<string> Dependencies => new[] { "ThemeCreator" };
+
+        protected override void Loaded(Harmony harmony)
+        {
+            LogInfo("Loading T2DTheme...");
+            // -- Fonts -- //
+            ThemeCreator.ThemeCreator.SetGlobalFont(new Font(FontFamily.GenericSansSerif, 11f));
+
+            // -- Colors -- //
+            // Black background.
+            ThemeCreator.ThemeCreator.AddGlobalColorSwap(Color.FromArgb(0, 0, 64), Color.Black);
+            // White foreground.
+            ThemeCreator.ThemeCreator.AddGlobalColorSwap(Color.FromArgb(255, 255, 192), Color.White);
+        }
+    }
+}
